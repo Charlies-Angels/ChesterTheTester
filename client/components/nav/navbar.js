e87d672d9b5dc76200cce90ac8719b7300086c6b
Caitlin Trussell <caitlintrussell@gmail.com>
2018-05-02 14:12:59 -0500

    actually a nav bar now

---
+++ client/components/nav/navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { progress: 25 };
+  }
+  render() {
+    return (
+      <div>
+        <div className="nav">
+          <div className="nav__title">
+            <h2>Chester Tester</h2>
+          </div>
+          <div className="nav__statusBar">
+            <div className="nav__meter">
+              {/* HERE Do a state check on progress to change the classname */}
+              <span className="nav__meter-25" />
+            </div>
+          </div>
+          <div className="nav__levelName">
+            <h2>Level One: Liftoff</h2>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
