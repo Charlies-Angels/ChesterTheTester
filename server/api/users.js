a16c2cdb21101e37c25af7308b0887d5f1ec7377
Roman <romanzalov@gmail.com>
2018-05-07 09:30:13 -0500

    models

--- server/api/users.js
+++ server/api/users.js
@@ -7,8 +7,16 @@ router.get('/', (req, res, next) => {
     // explicitly select only the id and email fields - even though
     // users' passwords are encrypted, it won't help if we just
     // send everything to anyone who asks!
-    attributes: ['id', 'email']
+    attributes: ['userId', 'email']
   })
     .then(users => res.json(users))
+    .catch(next)
+})
+
+router.get('/user/:id', (req, res, next) => {
+  User.findOne( {where: {userId: req.params.id},
+    attributes: [['firstName','lastName','email','maximumScore']]
+  }
+    .then(userId => res.json(userId))
     .catch(next)
 })
