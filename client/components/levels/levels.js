3e6747803197f1f00f5b3c457da374cfb57f1d52
Richard Liang <richardliang2015@u.northwestern.edu>
2018-05-08 15:58:57 -0500

    connected to levels

--- client/components/levels/levels.js
+++ client/components/levels/levels.js
@@ -1,7 +1,7 @@
 //Pure JS tests
 
 const levels = [
-	{
+{
 	level: 1,
 	title: `Lift Off!`,
 	objective: `Ok cadets, since no one checked the critical functions of the our intergalactic rocket, I guess it's up to us to make sure thing are going smoothly. We do unit testing here at Fullstack A-Cat-amy of Space, that means making many small tests to tests each part of our code.  `,
@@ -25,75 +25,76 @@ const levels = [
 	function:
 	`function mtrFrequency(meteorsInRange) {
 		const mtrAnalysis = meteorsInRange.reduce(function (meteorsInRange, type) {
-		 if (type in meteorsInRange) {
-			 meteorsInRange[type]++;
-		 }
-		 else {
-			 meteorsInRange[type] = 1;
-		 }
-		 return meteorsInRange;
-	 }, {});
-	 return mtrAnalysis;}
-
-
-	 var meteors = ["Iron meteor", "Iron meteor", "Stone meteor", "Stone meteor", "Stoney-iron meteor", "Stone meteor", "Stone meteor", "Stoney-iron meteor", "Iron meteor", "Undetermined meteor", "Iron meteor", "Iron meteor"];
-	 `
+			if (type in meteorsInRange) {
+				meteorsInRange[type]++;
+			}
+			else {
+				meteorsInRange[type] = 1;
+			}
+			return meteorsInRange;
+		}, {});
+		return mtrAnalysis;
+	}
+	var meteors = ["Iron meteor", "Iron meteor", "Stone meteor", "Stone meteor", "Stoney-iron meteor", "Stone meteor", "Stone meteor", "Stoney-iron meteor", "Iron meteor", "Undetermined meteor", "Iron meteor", "Iron meteor"];
+	`
 },
 {
 	level: 3,
 	objective: 'STORY',
 	function: `
-		const swap = (array, i, j) => {
+	const swap = (array, i, j) => {
 		let temp = array[i]
 		array[i] = array[j]
 		array[j] = temp
+	}
+
+	function spaceSort(array) {
+		let swaps = 0;
+		for (let i = 0; i < array.length - 1; i++) {
+			if (array[i] > array[i + 1]) {
+				swap(array, i, i + 1);
+				swaps++;
+			}
 		}
 
-		function spaceSort(array) {
-				let swaps = 0;
-				for (let i = 0; i < array.length - 1; i++) {
-						if (array[i] > array[i + 1]) {
-								swap(array, i, i + 1);
-								swaps++;
-						}
-				}
-
-				if (swaps > 0) {
-						return spaceSort(array);
-				}
-				return array;
-			}
+		if (swaps > 0) {
+			return spaceSort(array);
+		}
+		return array;
+	}
 
 
-		spaceSort([5, 2, 3, 4, 8, 7, 10])`
+	spaceSort([5, 2, 3, 4, 8, 7, 10])`
 },
 {
-level: 4,
-objective: 'STORY',
-function: `
-let Spacecraft = function(){};
-Spacecraft.prototype.fly = function(){
-	return 'they fly';
-};
+	level: 4,
+	objective: 'STORY',
+	function: `
+	let Spacecraft = function(){};
+	Spacecraft.prototype.fly = function(){
+		return 'they fly';
+	};
 
-let Rocket = function(){};
-Rocket.prototype = Object.create(Spacecraft.prototype);      //Rocket inherits from Spacecraft
-Rocket.prototype.blastOff = function(){
-	return 'blast off';
-};
+	let Rocket = function(){};
+	Rocket.prototype = Object.create(Spacecraft.prototype);      //Rocket inherits from Spacecraft
+	Rocket.prototype.blastOff = function(){
+		return 'blast off';
+	};
 
-let CatShip = function(){};
-CatShip.prototype = Object.create(Rocket.prototype)
+	let CatShip = function(){};
+	CatShip.prototype = Object.create(Rocket.prototype)
 	CatShip.info = ()=>{
-	return 'catShip: it remains ' + (CatShip.prototype instanceof Rocket) + ' that catShip is an instance of Spacecraft, therefore '+ CatShip.prototype.fly() + ' and ' + CatShip.prototype.blastOff()} + '!';
-};
+		return 'catShip: it remains ' + (CatShip.prototype instanceof Rocket) + ' that catShip is an instance of Spacecraft, therefore '+ CatShip.prototype.fly() + ' and ' + CatShip.prototype.blastOff()} + '!';
+	};
 
-console.log(CatShip.info())`
+	console.log(CatShip.info())`
 },
 {
-level: 5,
-objective: 'STORY',
-function: `setTimeout(() => {
+	level: 5,
+	objective: 'STORY',
+	function: `setTimeout(() => {
 		for (var x = 1; x <= 3; x++) alert(x) }, 1000);`
-}
+	}
 ]
+
+export default levels;
