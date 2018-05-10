b045f091822f5f212d48448194284ed69f155e5b
Roman <romanzalov@gmail.com>
2018-05-10 11:56:00 -0500

    navlink

--- client/components/level-layout/header.js
+++ client/components/level-layout/header.js
@@ -1,16 +1,23 @@
 import React from 'react';
-
+import {NavLink} from 'react-router-dom'
+import history from '../../history'
 const Header = (props) => {
   const levels = Array.from({length: 10}).map((x, i) => i);
   return (
       <div className="layout-header">
-        <div className="layout-header__left">{'<<<'}</div>
+        <div className="layout-header__left"><a href={`/level/${props.active-1}`}>{'<<<'}</a></div>
         <div className="layout-header__title">CHESTER TESTER</div>
         {levels.map(level => (
           props.active == (level + 1) ?
-          <div key={level} className="layout-header__levels-active">level {level + 1}</div> :
-          <div key={level} className="layout-header__levels">level {level + 1}</div>))}
-        <div className="layout-header__right">{'>>>'}</div>
+          // <NavLink key={level} to = {`/level/${level+1}`}>
+          <div key={level} className="layout-header__levels-active"> <a href={`/level/${level+1}`}>level {level + 1}</a></div> 
+          // </NavLink>
+          :
+          // <NavLink key={level} to = {`/level/${level+1}`}>
+          <div key={level} className="layout-header__levels"><a href={`/level/${level+1}`}>level {level + 1}</a></div>
+          // </NavLink>
+          ))}
+        <div className="layout-header__right"><a href={`/level/${props.active+1}`}>{'>>>'}</a></div>
       </div>
   )
 }
