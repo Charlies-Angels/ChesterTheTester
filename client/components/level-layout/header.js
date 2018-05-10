96aec1f8f3177d6ebec6415a0360fc8da2fd69e4
Caitlin Trussell <caitlintrussell@gmail.com>
2018-05-10 13:36:53 -0500

    i did some routing stuff

--- client/components/level-layout/header.js
+++ client/components/level-layout/header.js
@@ -3,21 +3,27 @@ import {NavLink} from 'react-router-dom'
 import history from '../../history'
 const Header = (props) => {
   const levels = Array.from({length: 10}).map((x, i) => i);
+  console.log(history)
+  const goBack = () => {
+    history.goBack();
+  }
+  const goToLevel = (level) => {
+    history.push(`/level/${level + 1}`)
+  }
+  const goForward = () => {
+    history.push(`/level/${+props.active + 1}`)
+  }
   return (
       <div className="layout-header">
-        <div className="layout-header__left"><a href={`/level/${props.active-1}`}>{'<<<'}</a></div>
+        <div className="layout-header__left" onClick={goBack} >{'<<<'}</div>
         <div className="layout-header__title">CHESTER TESTER</div>
         {levels.map(level => (
           props.active == (level + 1) ?
-          // <NavLink key={level} to = {`/level/${level+1}`}>
-          <div key={level} className="layout-header__levels-active"> <a href={`/level/${level+1}`}>level {level + 1}</a></div> 
-          // </NavLink>
+          <div key={level} className="layout-header__levels-active" onClick={() => (goToLevel(level))}>level {level + 1}</div>
           :
-          // <NavLink key={level} to = {`/level/${level+1}`}>
-          <div key={level} className="layout-header__levels"><a href={`/level/${level+1}`}>level {level + 1}</a></div>
-          // </NavLink>
+          <div key={level} className="layout-header__levels" onClick={() => (goToLevel(level))}>level {level + 1} </div>
           ))}
-        <div className="layout-header__right"><a href={`/level/${props.active+1}`}>{'>>>'}</a></div>
+        <div className="layout-header__right" onClick={goForward}>{'>>>'}</div>
       </div>
   )
 }
