3a271753dd265c49e2317384b20b8bae0c298deb
Roman <romanzalov@gmail.com>
2018-05-14 14:48:35 -0500

    postgres

--- server/db/db.js
+++ server/db/db.js
@@ -3,8 +3,9 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
+//const databaseURL= process.env.NODE_ENV === 'production' ? 'postgres://aa1jwzfcfpynafi.ctuvidhvqndl.us-east-1.rds.amazonaws.com:5432' :
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  process.env.DATABASE_URL || 'aa1jwzfcfpynafi.ctuvidhvqndl.us-east-1.rds.amazonaws.com:5432',
   {
     logging: false
   }
